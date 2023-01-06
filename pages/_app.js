b7b1e0de32cf69eb82e5dfa45b7f3bb669a01ad8
John-William Lotriet <john-william@plusplusminus.co.za>
2023-01-06 09:50:53 +0200

    base website

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '@components/Layout';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
-  );
+  )
 }
-
-export default MyApp;
